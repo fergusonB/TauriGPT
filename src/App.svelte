af0f094b25f87c743dc374e911001c880ab7baaf
Brenden <brenden.ferguson@gmail.com>
2023-04-07 13:54:02 -0700

    Added basic chat functionality to the application, allowing users to send and receive messages in real-time. This feature includes a user interface for composing and displaying messages, as well as backend logic for handling message transmission and storage.

--- src/App.svelte
+++ src/App.svelte
@@ -1,44 +1,138 @@
 <script>
-   import { invoke } from '@tauri-apps/api/tauri'
-  import Greet from './lib/Greet.svelte'
-  import SetApi from './lib/SetAPI.svelte';
+  let messages = [];
+  let currentMessage = '';
+  let apiKey = 'Replace this with your API key "sk-..."';
+  let openaiApiKey = ''; // Replace with your OpenAI API key
 
+  async function sendMessage() {
+  if (!currentMessage.trim()) return;
 
-  // Assume you have a Tauri instance set up
-// ...
+  const endpointUrl = 'https://api.openai.com/v1/chat/completions';
+  const model = 'gpt-3.5-turbo';
+  const messagesToSend = [{ role: 'user', content: currentMessage }];
+  
+  messages = [...messages, ...messagesToSend];
+  currentMessage = '';
 
+  while (true) {
+    const response = await fetch(endpointUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${openaiApiKey}`
+      },
+      body: JSON.stringify({ model, messages: messagesToSend })
+    });
 
-</script>
+    const data = await response.json();
+    const aiMessage = { role: 'ai', content:  data.choices[0].message.content };
+    messagesToSend.push(aiMessage);
 
-<main class="container">
-  <h1>Welcome to Tauri!</h1>
-
-  <div class="row">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-
-  <p>
-    Click on the Tauri, Vite, and Svelte logos to learn more.
-  </p>
-
-  <SetApi/>
-
-</main>
-
-<style>
-  .logo.vite:hover {
-    filter: drop-shadow(0 0 2em #747bff);
+    if (data.choices[0].finish_reason === 'stop') {
+      break;
+    }
   }
 
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00);
+  messages = [...messages, ...messagesToSend.slice(1)];
+}
+
+  function updateApiKey() {
+    openaiApiKey = apiKey;
+    apiKey = '';
+  }
+  
+</script>
+<div class="api-key-input">
+  <label for="api-key-field">API Key:</label>
+  <input type="text" id="api-key-field" bind:value={apiKey} />
+  <button on:click={updateApiKey}>Update</button>
+</div>
+
+  <div class="chat-container">
+    <div class="chat-messages">
+      {#each messages as message}
+        <div class="chat-message {message.role}">
+          <div class="chat-message-content">{message.content}</div>
+        </div>
+      {/each}
+    </div>
+    <div class="chat-input">
+      <form on:submit|preventDefault={sendMessage}>
+        <input type="text" bind:value={currentMessage} />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  </div>
+
+
+
+
+<style>
+  .chat-container {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+
+  .chat-messages {
+    flex: 1;
+    overflow-y: scroll;
+  }
+
+  .chat-message {
+    display: flex;
+    justify-content: flex-start;
+    margin: 10px;
+  }
+
+ 
+
+  .chat-message-content {
+    padding: 10px;
+    border-radius: 10px;
+    max-width: 80%;
+    word-break: break-word;
+  }
+
+  .user .chat-message-content {
+    background-color: #2ecc71;
+    color: #fff;
+  }
+
+  .ai .chat-message-content {
+    background-color: #f1f0f0;
+    color: #333;
+  }
+
+  .chat-input {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 10px;
+  }
+
+  .chat-input form {
+    display: flex;
+    flex: 1;
+    margin-right: 10px;
+  }
+
+  .chat-input input {
+    flex: 1;
+    padding: 10px;
+    border-radius: 5px;
+    border: none;
+    font-size: 16px;
+    outline: none;
+  }
+
+  .chat-input button {
+    padding: 10px;
+    border-radius: 5px;
+    border: none;
+    background-color: #3498db;
+    color: #fff;
+    font-size: 16px;
+    cursor: pointer;
   }
 </style>
